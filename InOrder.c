caa18496473c8ad7e4d898b544b381b17a78098d
Alfi Zulfian Abdi <Alfi Zulfian Abdi>
2016-08-16 22:45:27 +0700

    Void Tambah

--- InOrder.c
+++ InOrder.c
@@ -7,3 +7,22 @@ typedef struct Node{
       Node *kanan; 
 };
 
+void tambah(Node **root, int databaru){
+      if((*root) == NULL){       
+            Node *baru;
+            baru = new Node;
+            baru->data = databaru; 
+            baru->kiri = NULL;
+            baru->kanan = NULL;
+            (*root) = baru; 
+            (*root)->kiri = NULL;
+            (*root)->kanan = NULL; 
+            printf("Data bertambah!");
+      }
+      else if(databaru < (*root)->data)
+            tambah(&(*root)->kiri, databaru);
+      else if(databaru > (*root)->data)
+            tambah(&(*root)->kanan, databaru); 
+      else if(databaru == (*root)->data)
+            printf("Data sudah ada!");
+}
